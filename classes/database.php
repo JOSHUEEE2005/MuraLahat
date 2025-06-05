3151c9f50049b68e824b29c8b6765feace72605c
johannesclaus30 <clausja@students.nu-lipa.edu.ph>
2025-06-05 08:03:38 +0800

    commit 3 - Working Add Products

--- classes/database.php
+++ classes/database.php
@@ -1,14 +1,19 @@
 <?php
-class database {
-    function opencon(): PDO {
-        return new PDO(
-            dsn: 'mysql:host=localhost;dbname=muralahat',
-            username: 'root',
-            password: ''
-        );
-    }
+    class database {
+        function opencon(): PDO {
 
-    function signupUser($username, $password, $position, $profile_picture_path) {
+            //pachange nalang nito if iba ang name ng db niyo
+            return new PDO('mysql:host=localhost;
+            dbname=ml_dbs',
+            username: 'root',
+            password: '');
+
+            require_once('classes/database.php');
+            $con = new database();
+            $data = $con->opencon();
+        }
+
+        function signupUser($username, $password, $position, $profile_picture_path) {
         $con = $this->opencon();
         
         try {
@@ -51,5 +56,53 @@ class database {
             return false;
         }
     }
-}
+
+        
+        function addNewProduct($user_account_id, $prod_name, $prod_quantity, $prod_price, $date_added, $price_effective_from, $price_effective_to, $category_ids = []) {
+                $con = $this->opencon();
+                try {
+                    $con->beginTransaction();
+
+                    //Insert into Product table
+                    $stmt = $con->prepare("INSERT INTO Product (User_Account_ID, Product_Name, Product_Stock) VALUES (?,?,?)");
+                    $stmt->execute([$user_account_id, $prod_name, $prod_quantity]);
+                    
+                    //Get the newly inserted product_id
+                    $product_id = $con->lastInsertId();
+
+                    // Insert into Product_Price table
+                    $stmt = $con->prepare("INSERT INTO Product_Price (Product_ID, Price, Effective_From, Effective_To, Created_At) VALUES (?,?,?,?,?)");
+                    $stmt->execute([$product_id, $prod_price, $price_effective_from, $price_effective_to, $date_added]);
+                        
+                    // Insert into Product_Category table
+                    foreach ($category_ids as $category_id) {
+                    $stmt = $con->prepare("INSERT INTO Product_Category (Product_ID, Category_ID) VALUES (?,?)");
+                    $stmt->execute([$product_id, $category_id]);
+                    }
+
+                    $con->commit();
+                    return $product_id;
+                } catch (PDOException $e) {
+                    $con->rollBack();
+                    return false; // Update failed
+                }
+        }
+
+        function viewCategory() {
+            $con = $this->opencon();
+            return $con->query("SELECT * FROM Category")
+            ->fetchAll();
+        }
+
+        function viewCategoryID($id) {
+            $con = $this->opencon();
+            $stmt = $con->prepare("SELECT * FROM Category WHERE Category_ID = ?");
+            $stmt->execute([$id]);
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+
+
+    }
+    
+    
 ?>
